[Python] i) move normalization outside trainers; ii) fix misalignment with MATLAB; iii) move the saver to main function and pass it to trainers

Python/newton_cg.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 import os
 import math
-from utilities import predict, normalize_and_reshape
+from utilities import predict
 
 def Rop(f, weights, v):
 	"""Implementation of R operator
@@ -316,7 +316,7 @@ class newton_cg(object):
 		return (CG_ops(), update_v())
 
 
-	def newton(self, full_batch, val_batch, network, test_network=None):
+	def newton(self, full_batch, val_batch, saver, network, test_network=None):
 		"""
 		Conduct newton steps for training
 		args:
@@ -330,19 +330,11 @@ class newton_cg(object):
 		"""
 		# check whether data is valid
 		full_inputs, full_labels = full_batch
-		val_inputs, val_labels = val_batch
 		assert full_inputs.shape[0] == full_labels.shape[0]
 
 		if full_inputs.shape[0] != self.config.num_data:
 			raise ValueError('The number of full batch inputs does not agree with the config argument.\
 							This is important because global loss is averaged over those inputs')
-
-		# normalize images and reshape them 
-		mean_tr = full_inputs.mean(axis=0)
-		full_inputs = normalize_and_reshape(full_inputs, dim=self.config.dim, mean_tr=mean_tr)
-		val_inputs = normalize_and_reshape(val_inputs, dim=self.config.dim, mean_tr=mean_tr)
-		full_batch = (full_inputs, full_labels)
-		val_batch = (val_inputs, val_labels)
 
 		x, y, _, outputs = network
 
@@ -364,13 +356,6 @@ class newton_cg(object):
 		total_running_time = 0.0
 		self.config.elapsed_time = 0.0
 		total_CG = 0
-		
-		# Initialize a mean_param. The tensor is for future prediction in predict.py.
-		mean_param = tf.get_variable(name='mean_tr', initializer=mean_tr)
-		self.sess.run(tf.variables_initializer([mean_param]))
-
-		# saver = tf.train.Saver(var_list=self.param)
-		saver = tf.train.Saver(var_list=self.param+[mean_param])
 		
 		for k in range(self.config.iter_max):
 

Python/predict.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 		imported_graph.restore(sess, args.model_file)
 		mean_param=tf.get_default_graph().get_tensor_by_name('mean_tr:0')
 		mean_tr = sess.run(mean_param)
-		test_batch[0] = normalize_and_reshape(test_batch[0], dim=args.dim, mean_tr=mean_tr)
+		test_batch[0], _ = normalize_and_reshape(test_batch[0], dim=args.dim, mean_tr=mean_tr)
 
 		x = tf.get_default_graph().get_tensor_by_name('main_params/input_of_net:0')
 		y = tf.get_default_graph().get_tensor_by_name('main_params/labels:0')

Python/train.py
@@ -7,7 +7,7 @@ import argparse
 
 from net.net import CNN
 from newton_cg import newton_cg
-from utilities import read_data, predict, ConfigClass
+from utilities import read_data, predict, ConfigClass, normalize_and_reshape
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='Newton method on DNN')
@@ -109,7 +109,7 @@ def init_model(param):
 		init_ops.append(opt)
 	return tf.group(*init_ops)
 
-def gradient_trainer(config, sess, network, full_batch, val_batch, test_network):
+def gradient_trainer(config, sess, network, full_batch, val_batch, saver, test_network):
 	x, y, loss, outputs,  = network
 	
 	global_step = tf.Variable(initial_value=0, trainable=False, name='global_step')
@@ -140,15 +140,7 @@ def gradient_trainer(config, sess, network, full_batch, val_batch, test_network)
 								global_step=global_step,
 								colocate_gradients_with_ops=True)
 
-	full_inputs, full_labels = full_batch
-	val_inputs, val_labels = val_batch
-	# normalize images and reshape them 
-	mean_tr = full_inputs.mean(axis=0)
-	full_inputs = normalize_and_reshape(full_inputs, dim=self.config.dim, mean_tr=mean_tr)
-	val_inputs = normalize_and_reshape(val_inputs, dim=self.config.dim, mean_tr=mean_tr)
-	full_batch = (full_inputs, full_labels)
-	val_batch = (val_inputs, val_labels)
-	
+	train_inputs, train_labels = full_batch
 	num_data = train_labels.shape[0]
 	batch_size = config.bsize
 	num_iters = math.ceil(num_data/batch_size)
@@ -166,11 +158,6 @@ def gradient_trainer(config, sess, network, full_batch, val_batch, test_network)
 
 	total_running_time = 0.0
 	best_acc = 0.0
-
-	# Initialize a mean_param. The tensor is for future prediction in predict.py.
-	mean_param = tf.get_variable(name='mean_tr', initializer=mean_tr)
-	self.sess.run(tf.variables_initializer([mean_param]))
-	saver = tf.train.Saver(var_list=param+[mean_param])
 
 	for epoch in range(0, args.epoch):
 		
@@ -260,7 +247,7 @@ def gradient_trainer(config, sess, network, full_batch, val_batch, test_network)
 		print(output_str, file=log_file)
 		log_file.close()
 
-def newton_trainer(config, sess, network, full_batch, val_batch, test_network):
+def newton_trainer(config, sess, network, full_batch, val_batch, saver, test_network):
 
 	_, _, loss, outputs = network
 	newton_solver = newton_cg(config, sess, outputs, loss)
@@ -269,7 +256,7 @@ def newton_trainer(config, sess, network, full_batch, val_batch, test_network):
 	print('-------------- initializing network by methods in He et al. (2015) --------------')
 	param = tf.trainable_variables()
 	sess.run(init_model(param))
-	newton_solver.newton(full_batch, val_batch, network, test_network)
+	newton_solver.newton(full_batch, val_batch, saver, network, test_network)
 
 
 def main():
@@ -307,13 +294,20 @@ def main():
 	sess_config.gpu_options.allow_growth = False
 
 	with tf.Session(config=sess_config) as sess:
+		
+		full_batch[0], mean_tr = normalize_and_reshape(full_batch[0], dim=config.dim, mean_tr=None)
+		val_batch[0], _ = normalize_and_reshape(val_batch[0], dim=config.dim, mean_tr=mean_tr)
+
+		param = tf.trainable_variables()
+		mean_param = tf.get_variable(name='mean_tr', initializer=mean_tr, trainable=False)
+		saver = tf.train.Saver(var_list=param+[mean_param])
 
 		if config.optim in ('SGD', 'Adam'):
 			gradient_trainer(
-				config, sess, network, full_batch, val_batch, test_network)
+				config, sess, network, full_batch, val_batch, saver, test_network)
 		elif config.optim == 'NewtonCG':
 			newton_trainer(
-				config, sess, network, full_batch, val_batch, test_network=test_network)
+				config, sess, network, full_batch, val_batch, saver, test_network=test_network)
 
 
 if __name__ == '__main__':

Python/utilities.py
@@ -83,6 +83,7 @@ def read_data(filename):
 	images, labels = mat_contents['Z'], mat_contents['y']
 	
 	labels = labels.reshape(-1)
+	images = images.reshape(images.shape[0], -1)
 
 	# check data validity
 	label_enum = list(dict.fromkeys(labels))
@@ -113,7 +114,6 @@ def normalize_and_reshape(images, dim, mean_tr=None):
 
 	if mean_tr is None:
 		print('No mean of data provided! Normalize images by their own mean.')
-		mean_tr = images.mean(axis=0)
 	else:
 		print('Normalzie images according to the provided mean.')
 		if np.prod(mean_tr.shape) != np.prod(dim):
@@ -125,14 +125,18 @@ def normalize_and_reshape(images, dim, mean_tr=None):
 	max_value = images.max(axis=1).reshape(-1,1)
 	min_value = images.min(axis=1).reshape(-1,1)
 	
-	images = images - mean_tr
 	images = (images - min_value) / (max_value - min_value)
+
+	if mean_tr is None:
+		# if no mean_tr is provided, we calculate it according to the current data
+		mean_tr = images.mean(axis=0) 
+	images = images - mean_tr
 
 	images = images.reshape(images_shape)
 	# Tensorflow accepts data shape: B x H x W x C
 	images = np.transpose(images, (0, 2, 3, 1))
 
-	return images
+	return images, mean_tr
 
 
 def predict(sess, network, test_batch, bsize):