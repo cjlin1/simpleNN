1b6c744434792cab3d6d0fd2efde0e6f344655fb
Chien-Chih Wang <d98922007@ntu.edu.tw>
2020-05-17 11:05:25 -0700

    polish maxpooing.m

--- MATLAB/cnn/maxpooling.m
+++ MATLAB/cnn/maxpooling.m
@@ -1,32 +1,24 @@
-function [Zout, idx_pool, R_Z] = maxpooling(model, net, Z, m, task)
+function [Z, idx_pool, R_Z] = maxpooling(model, net, Zin, m, R_Zin)
 
 a = model.ht_conv(m);
 b = model.wd_conv(m);
 d = model.ch_input(m+1);
 h = model.wd_subimage_pool(m);
 
-if exist('task', 'var') && ~any(strcmp(task, {'R'}))
-	error('We do not support this pooling type');	
-end
-
 P = net.idx_phiZ_pool{m};
-Z = reshape(Z, d*a*b, []);
+Z = reshape(Zin, d*a*b, []);
 Z = Z(P, :);
 Z = reshape(Z, h*h, []);
 
-if exist('task', 'var') && strcmp(task, 'R')
-	R_Z = Z(:, end/2+1:end);
-	Z = Z(:, 1:end/2);
-end
-
 [Z, max_id] = max(Z);
-Zout = reshape(Z, d, []);
+Z = reshape(Z, d, []);
 
-if exist('task', 'var') && strcmp(task, 'R')
-	R_max_id = reshape(max_id + h*h*gpu([0:size(Z, 2)-1]), d, []);
-	R_Z = R_Z(R_max_id);
-end
 outa = model.ht_input(m+1);
 outb = model.wd_input(m+1);
 max_id = reshape(max_id, d*outa*outb, []) + h*h*gpu([0:d*outa*outb-1])';
-idx_pool = P(max_id);
+idx_pool = P(max_id) + gpu([0:size(max_id, 2)-1])*d*a*b;
+
+if exist('R_Zin', 'var')
+	R_Z = reshape(R_Zin, d*a*b, []);
+	R_Z = reshape(R_Z(idx_pool), d, []);
+end
